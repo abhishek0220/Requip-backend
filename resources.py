ab5ab14a52083fa552c327646ef3e43a763bd453
abhyamgupta123 <abhyamgupta123@gmail.com>
2020-11-13 01:49:57 +0530

    user login added

--- resources.py
+++ resources.py
@@ -9,16 +9,19 @@ class UserRegistration(Resource):
         parser.add_argument('username', help = 'This field cannot be blank', required = True)
         parser.add_argument('email', help = 'This field cannot be blank', required = True)
         parser.add_argument('password', help = 'This field cannot be blank', required = True)
+        parser.add_argument('phone_number', help = 'This field can be blank', required = False)
         data = parser.parse_args()
         username = data['username']
         email = data['email']
         password = data['password']
+        phone_number = data['phone_number']
         if(db.users.find_one({'email': email}) or db.users.find_one({'username' : username})):
             return {'message': 'User already exists'}
         user = {
             'username' : username,
             'email' : email,
-            'password' : password
+            'password' : password,
+            'phone_number' : phone_number
         }
         try:
             db.users.insert(user)
@@ -33,3 +36,26 @@ class UserRegistration(Resource):
             'refresh_token': refresh_token
             }
 
+class User_login(Resource):
+    def __init__(self):
+        pass
+
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('email_or_username', help = 'This field can be blank', required = False)
+        parser.add_argument('password', help = 'This field cannot be blank', required = True)
+        data = parser.parse_args()
+        email_or_username = data['email_or_username']
+        password = data['password']
+        phone_number = data['phone_number']
+
+        if(db.users.find_one({'email': email_or_username}) or db.users.find_one({'username' : email_or_username})):
+            access_token = create_access_token(identity = email_or_username)
+            refresh_token = create_refresh_token(identity = email_or_username)
+            return {
+            'message': 'Logging in User {}'.format(data['username']),
+            'access_token': access_token,
+            'refresh_token': refresh_token
+            }
+        else:
+            return {'message': 'User does not exists'}
