acbab195d1e6e1e0b7f85dc273357953340dc184
abhyamgupta123 <abhyamgupta123@gmail.com>
2020-11-13 15:47:19 +0530

    added userprofile

--- Requip/__init__.py
+++ Requip/__init__.py
@@ -18,3 +18,4 @@ db = mongo.db
 from Requip.Resources import user
 api.add_resource(user.UserRegistration, '/registration')
 api.add_resource(user.UserLogin, '/login')
+api.add_resource(user.UserProfile, '/profile')
